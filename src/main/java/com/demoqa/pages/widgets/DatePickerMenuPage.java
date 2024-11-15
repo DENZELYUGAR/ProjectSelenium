f83f621694872a56e40177c972b4c20a712731a2
DENZELYUGAR <yugarcaceresdenzel@gmail.com>
2024-11-14 23:52:48 -0400

    Work With Dates

--- src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
+++ src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
@@ -10,6 +10,22 @@ public class DatePickerMenuPage extends WidgetsPage{
     private By monthDropDown = By.className("react-datepicker__month-select");
     private By yearDropDown = By.cssSelector(".react-datepicker__year-select");
 
+    private By dayValue(String day) {
+        return By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day--')][text()='"+ day +"']");
+    }
+
+    public void clickDay(String day) {
+        //By dayValue = By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day--')][text()='"+ day +"']");
+        //click(dayValue);
+       click(dayValue(day));
+    }
+
+    public boolean isDayInMonth(String day) {
+        //By dayValue = By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day--')][text()='"+ day +"']");
+        //return find(dayValue).isDisplayed();
+        return find(dayValue(day)).isDisplayed();
+    }
+
     public void clickSelectDate() {
         click(selectDateField);
     }
