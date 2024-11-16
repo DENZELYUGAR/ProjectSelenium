893956280f7dcaaa13e6f94021a866d93e06ec20
DENZELYUGAR <yugarcaceresdenzel@gmail.com>
2024-11-15 21:25:05 -0400

    Screenshot

--- src/test/java/part3_4/com/demoqa/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/demoqa/base/BaseTest.java
@@ -2,11 +2,17 @@ package part3_4.com.demoqa.demoqa.base;
 
 import com.demoqa.pages.HomePage;
 import com.base.BasePage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
 
 import static com.base.BasePage.delay;
 import static utilities.Utility.setUtilityDriver;
@@ -31,6 +37,15 @@ public class BaseTest {
         basePage.setDriver(driver);
         setUtilityDriver();
         homePage = new HomePage();
+    }
+
+    @AfterMethod
+    public void takeFailedResultScreenshot(ITestResult testResult) {
+        if (ITestResult.FAILURE == testResult.getStatus()) {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File source = screenshot.getScreenshotAs(OutputType.FILE);
+            File destination = new File(System.getProperty("user.dir") + "/resources/screenshots/(" + )
+        }
     }
 
     @AfterClass
