29f138efc196d8a0822e41d2515112de775b8c19
DENZELYUGAR <yugarcaceresdenzel@gmail.com>
2024-11-24 23:45:20 -0400

    *

--- src/test/java/part3_4/com/demoqa/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/demoqa/tests/part4/alerts/AlertsTest.java
@@ -1,12 +1,28 @@
 package part3_4.com.demoqa.demoqa.tests.part4.alerts;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import part3_4.com.demoqa.demoqa.base.BaseTest;
 
+import static utilities.SwitchToUtility.*;
+
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert() {
-        var alersPage = homePage.goToAlertsFramesWindowsCard();
+        String expectedAlertText = "You clicked a button";
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickInformationAlertButton();
+        Assert.assertEquals(getAlertText(), expectedAlertText, "\n Actual & Expected Messages Do Not Match \n");
+        acceptAlert();
+    }
+
+    public void testConfirmationAlert() {
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        String expectedConfirmationResult = "You selected Cancel";
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult, "\n You Did Not Select Cancel \n");
     }
 }
